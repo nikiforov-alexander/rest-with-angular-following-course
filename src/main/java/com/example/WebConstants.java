9af8f97b4d2649fe19f238d05e5545c785b4490d
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-25 15:48:04 +0100

    Add WebConstants class with 3 new constants
    API_CONTEXT, CONTENT_TYPE and INDEX_PAGE_ALL_TASKS.
    
    They are useful, all public for now, They are useful because I use them in tests and in Main method, at least

---
+++ src/main/java/com/example/WebConstants.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class WebConstants {
+    public static final String API_CONTEXT = "/api/v1";
+    public static final String CONTENT_TYPE = "application/json";
+    public static final String INDEX_PAGE_ALL_TASKS = API_CONTEXT + "/tasks";
+}
