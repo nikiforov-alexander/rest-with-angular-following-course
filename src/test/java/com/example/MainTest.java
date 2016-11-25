e92b961b9e9a785012fa0d532590b882a4fefc9f
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-25 15:50:25 +0100

    Add MainTest class with index page test
    Only index page is tested, setup is taken from old methods, all 4 setup methods are used to correctly use testing database, and testing port for our tests.
    
    First test simply checks length of tasks returned and status

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,85 @@
+package com.example;
+
+import com.example.dao.TaskDao;
+import com.example.model.Task;
+import com.example.test_helpers.ApiClient;
+import com.example.test_helpers.ApiResponse;
+import com.google.gson.Gson;
+import org.junit.*;
+import spark.Spark;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static com.example.WebConstants.*;
+
+public class MainTest {
+
+    // members used in tests
+
+    private static final String PORT = "4568";
+    private ApiClient apiClient;
+    private Gson gson;
+    private TaskDao taskDao;
+
+    // Test preparation methods
+
+    @BeforeClass
+    public static void startServer() {
+        // run our App with other than in real App port and JDBC url
+        // testing database
+        String[] args = {PORT, "hibernate-test.cfg.xml"};
+        Main.main(args);
+
+        // wait before Spark server is up
+        Spark.awaitInitialization();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        apiClient = new ApiClient("http://localhost:" + PORT);
+        taskDao = Main.getTaskDao();
+        gson = Main.getGson();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // close database connection
+        taskDao.closeDatabase();
+    }
+
+    // Stop Spark Server after all tests
+    @AfterClass
+    public static void stopServer() {
+        Spark.stop();
+    }
+
+    // private helper methods used in tests
+    private void addTestTasksToDatabase(int numberOfTasks) {
+        for (int i = 0; i < numberOfTasks; i++) {
+            Task task = new Task();
+            task.setName("task " + i);
+            taskDao.saveOrUpdate(task);
+        }
+    }
+
+    // actual tests
+
+    @Test
+    public void getRequestToListAllTasksWorks() throws Exception {
+        // Given 5 added test tasks to database
+        addTestTasksToDatabase(5);
+
+        // When GET request to INDEX_PAGE_ALL_TASKS is made
+        ApiResponse apiResponse =
+                apiClient.request("GET",
+                        INDEX_PAGE_ALL_TASKS);
+
+        // Then status should be 200
+        assertThat(apiResponse).hasFieldOrPropertyWithValue(
+                "status", 200
+        );
+
+        // Then 5 tasks should be returned
+        Task[] tasks = gson.fromJson(apiResponse.getBody(), Task[].class);
+        assertThat(tasks.length).isEqualTo(5);
+    }
+}
