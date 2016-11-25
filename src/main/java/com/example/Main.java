28e178ade3b4615054351154b001624088742a3d
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-25 15:44:16 +0100

    Add initialization part to Main and first GET index page request
    Main reason for such method is driven from Hibernate + Spark + Testing conceptions:
    
    "taskDao" and "gson" are defined "static" with getters, so that we can use them in tests.
    
    "Main.main" accepts two arguments: PORT and hibernate configuration file. That is used purely in testing, to load testing database with another PORT number.
    
    GET request to index page uses WebConstants class, that is proved to be useful, to keep track of the constants used here and there.
    
    This "Main" class is tested for GET method for index page

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,52 @@
 package com.example;
 
+import com.example.dao.TaskDao;
+import com.example.dao.TaskDaoImpl;
+import com.google.gson.Gson;
+
+import static com.example.WebConstants.CONTENT_TYPE;
+import static com.example.WebConstants.INDEX_PAGE_ALL_TASKS;
+import static spark.Spark.*;
+
 public class Main {
+
+    private static TaskDao taskDao;
+    private static Gson gson = new Gson();
+
+    static TaskDao getTaskDao() {
+        return taskDao;
+    }
+
+    static Gson getGson() {
+        return gson;
+    }
 
     public static void main(String[] args) {
 
+        // initialization part
+
+        String hibernateConfigurationFile = "hibernate.cfg.xml";
+
+        if (args.length > 0) {
+            if (args.length != 2) {
+                System.out.println("java Api <port> <hibernate.cfg.xml>");
+                System.exit(1);
+            }
+            // it is not supposed to run other than testing
+            // so we won't check for NumberFormatException
+            port(Integer.parseInt(args[0]));
+            hibernateConfigurationFile = args[1];
+        }
+
+        taskDao = new TaskDaoImpl(hibernateConfigurationFile);
+
+        // actual request methods
+
+        // index page: GET all tasks
+        get(INDEX_PAGE_ALL_TASKS,
+                CONTENT_TYPE,
+                (request, response) -> taskDao.findAll(),
+                gson::toJson
+        );
     }
 }
