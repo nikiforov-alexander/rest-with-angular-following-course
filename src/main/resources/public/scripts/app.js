64d8814741c35ce438ebbe600120261186efb3bc
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-28 11:15:37 +0100

    Add service and connect with REST for index page
    'dataService' was introduced that makes request to server, and upon successful get of data, it assigns obtained tasks to $scope.tasks and prints them as it should be.
    
    Old mock tasks were removed.

--- src/main/resources/public/scripts/app.js
+++ src/main/resources/public/scripts/app.js
@@ -2,7 +2,9 @@
 // empty array
 angular.module("tasksListApp", [])
 
-.controller('MainController', function ($scope) {
+// IMPORTANT : we have to add 'dataService' here, otherwise
+// we'll get "can't find dataService" error
+.controller('MainController', function ($scope, dataService) {
 
    // we set editing to false so that IDE
    // also sees it, and I think it is a good practice
@@ -12,11 +14,30 @@ angular.module("tasksListApp", [])
         console.log("adding Task");
    };
 
-   $scope.tasks = [
-       {"name" : "Clean the House"},
-       {"name" : "Wash the Dishes"},
-       {"name" : "Call the Owner"},
-       {"name" : "Create the App"},
-       {"name" : "Test the App"}
-   ]
+   dataService.getTasks(
+       function (response) {
+            console.log(response.data);
+            $scope.tasks = response.data;
+       }
+   );
+
+   // writing here
+   // $scope.tasks = dataService.getTasks();
+   // will NOT work, because this code is executed
+   // before request is made
+
+})
+// get tasks from server, service
+.service('dataService', function ($http) {
+
+    // here we define getTasks as a function that
+    // takes callback as argument, and in controller
+    // we pass "response" as argument = callback
+    // because of nature oj AJAX requests
+    // NOTE : we cannot use 'response' object with data
+    // here directly
+    this.getTasks = function(callback){
+        $http.get('/api/v1/tasks')
+            .then(callback)
+    }
 });
